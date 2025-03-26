e2320eaecba9e8bcb21ecd9e4fad3f64f6646c79
NoB0T21 <147228189+NoB0T21@users.noreply.github.com>
2025-03-27 04:23:31 +0530

    u

--- Notes/backend/server.js
+++ Notes/backend/server.js
@@ -2,9 +2,13 @@ import express from 'express';
 const app = express();
 import cors from 'cors'
 import path from 'path'
+import { fileURLToPath } from 'url';
 
 import dotenv from 'dotenv';
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import connectToDB from './db/db.js'
 connectToDB();
