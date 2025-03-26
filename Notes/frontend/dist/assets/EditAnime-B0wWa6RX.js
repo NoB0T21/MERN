becbdf80c0eed6dc482e908d9b8e0cc80767ae2b
NoB0T21 <147228189+NoB0T21@users.noreply.github.com>
2025-03-27 05:02:08 +0530

    u

---
+++ Notes/frontend/dist/assets/EditAnime-B0wWa6RX.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Edit-BKnLZ4B5.js","assets/index-BMTxBsQZ.js","assets/index-CLQgUgOb.css"])))=>i.map(i=>d[i]);
+import{j as t,A as i,m as e,r as a,_ as s}from"./index-BMTxBsQZ.js";const r=a.lazy(()=>s(()=>import("./Edit-BKnLZ4B5.js"),__vite__mapDeps([0,1,2]))),o=()=>t.jsx(i,{children:t.jsx(e.div,{className:"w-full h-full",initial:{y:-200},animate:{y:0},exit:{y:200},transition:{duration:.5,ease:[.22,1,.36,1]},children:t.jsx(r,{})})});export{o as default};
--- Notes/frontend/src/utils/api.js
+++ Notes/frontend/src/utils/api.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const URL='https://notes-pi-amber.vercel.app'
 
-export const api = axios.create({baseURL: import.meta.env.VITE_URI});
+export const api = axios.create({baseURL: URL});
 
 const usenote = (setNotesData) => {
   const getnote = async () => {
-    const note = await axios.get(`${import.meta.env.VITE_URI}/getnotes`);
+    const note = await axios.get(`${URL}/getnotes`);
     setNotesData(note.data);
   };
   return{getnote};
