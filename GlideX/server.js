b66f6a7d346d2531543c984b6b7538202f84deda
NoB0T21 <147228189+NoB0T21@users.noreply.github.com>
2025-07-01 14:50:33 +0530

    Delete GlideX directory

--- GlideX/server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const app = require("./app");
-const port = process.env.PORT;
-
-const server = http.createServer(app);
-
-server.listen(port, () => {
-    console.log(`server is running on port ${port}`);
-});
